e0d00f3611118f6ceceaf2f88c4899888c5d150c
case-buttitta <cbuttitt@charlotte.edu>
2026-02-05 15:39:48 -0500

    added in dev and production profiles to docker for hot-reload of react frontend on save. Also fixed white on white issue in user table

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,          // Expose to Docker host
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://backend:5000', // Docker service name
+        changeOrigin: true
+      }
+    }
+  }
 })
