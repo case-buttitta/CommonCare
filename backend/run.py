4ed8fdbd94e03cc0ead3deac8970a714488eb75c
Thomas Powell <72366532+Thpow@users.noreply.github.com>
2026-03-24 16:00:03 -0400

    use aiven correctly

--- backend/run.py
+++ backend/run.py
@@ -132,5 +132,5 @@ def seed_data():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-        # seed_data() # Disabled for persistent cloud DB
+        seed_data()
     app.run(debug=True, host='0.0.0.0', port=5000)
